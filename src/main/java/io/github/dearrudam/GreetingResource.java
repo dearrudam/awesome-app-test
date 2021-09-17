228581a465d6d1d32a2255f40f2bb887ed9855c3
Maximillian Arruda <dearrudam@gmail.com>
2021-09-17 17:25:55 -0300

    First commit

--- src/main/java/io/github/dearrudam/GreetingResource.java
+++ src/main/java/io/github/dearrudam/GreetingResource.java
@@ -3,14 +3,32 @@ package io.github.dearrudam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/hello")
 public class GreetingResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    @Produces(MediaType.TEXT_HTML)
+    public String hello(
+        @QueryParam("nome") final String nome
+        ) {
+        return "<html><body><h1> olá " + escapeHTML(nome) + "!</h1></body></html>";
     }
+    
+    private String escapeHTML(String s){
+        StringBuilder out = new StringBuilder(Math.max(16, s.length()));
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c > 127 || c == '"' || c == '\'' || c == '<' || c == '>' || c == '&') {
+                out.append("&#");
+                out.append((int) c);
+                out.append(';');
+            } else {
+                out.append(c);
+            }
+        }
+        return out.toString();
+    } 
 }
