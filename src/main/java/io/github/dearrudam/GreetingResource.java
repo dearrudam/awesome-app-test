611f306d3388e38d1ff13a792ce78887e0e90863
Maximillian Arruda <dearrudam@gmail.com>
2021-10-15 23:11:45 -0300

    Fixed the XSS Injection code sample

--- src/main/java/io/github/dearrudam/GreetingResource.java
+++ src/main/java/io/github/dearrudam/GreetingResource.java
@@ -1,6 +1,10 @@
 package io.github.dearrudam;
 
 import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
@@ -9,23 +13,25 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+
 import org.jboss.resteasy.plugins.server.servlet.HttpServletResponseWrapper;
 import org.jboss.resteasy.spi.HttpRequest;
 
-@Path("/hello")
-public class GreetingResource {
-    @GET
-    @Produces(MediaType.TEXT_HTML)
-    public void hello(
-        @Context HttpServletRequest request,
-        @Context HttpServletResponse response
-        ) throws IOException {
+@WebServlet(urlPatterns = {"/hello"})
+public class GreetingResource extends HttpServlet {
 
-        response.getWriter().write( ""
-        + "<html>"
-        + "    <body>"
-        + "        <h1> Olá " + request.getParameter("nome") + "!</h1>"
-        + "    </body>"
-        + "</html>");
+    @Override
+    protected void doGet(
+            HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.getWriter().write(""
+                + "<html>"
+                + "    <body>"
+                + "        <h1> Olá " + request.getParameter("nome") + "!</h1>"
+                + "    </body>"
+                + "</html>");
+
+        response.setHeader("Set-Cookie","TOKEN=12312312321312");
     }
 }
